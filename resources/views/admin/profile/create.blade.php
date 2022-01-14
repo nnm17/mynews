a4da863496625fc987341a5d73db19eaf0e70fb1
kanekonanami <n-kaneko@jutjoy>
2022-01-14 16:02:06 +0900

    4-15

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -11,6 +11,45 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>プロフィール新規作成</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">氏名</label>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                    <label class="form-check-label" for="release1">性別</label>
+                        <div class="form-check form-check-inline">
+                            <input type="radio" class="form-check-input" name="release" value="男性" {{ old ('release') == '男性' ? 'checked' : '' }}>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input type="radio" class="form-check-input" name="release" value="女性" {{ old ('release') == '女性' ? 'checked' : '' }}>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="body">趣味</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="body" rows="8">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="body">自己紹介</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="body" rows="12">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
             </div>
         </div>
     </div>
