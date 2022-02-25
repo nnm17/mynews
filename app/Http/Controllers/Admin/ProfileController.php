cca96d40a2764f233702711ffc968001092c35fb
kanekonanami <n-kaneko@jutjoy>
2022-02-25 14:32:23 +0900

    4-16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,27 +1,41 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// 以下を追記することでNews Modelが扱えるようになる
+use App\Profile;
+
 class ProfileController extends Controller
 {
+
     public function add()
     {
         return view('admin.profile.create');
     }
-    
-    public function create()
+
+    public function create(Request $request)
     {
-        return redirect('admin/profile/create');
+      // 以下を追記
+      // Varidationを行う
+      $this->validate($request, Profile::$rules);
+
+      $profile = new Profile;
+      $form = $request->all();
+
+      // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+
+      return redirect('admin/profile/create');
     }
-    
+
     public function edit()
     {
         return view('admin.profile.edit');
     }
-    
+
     public function update()
     {
         return redirect('admin/profile/edit');
